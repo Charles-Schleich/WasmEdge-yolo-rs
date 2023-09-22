5362dfa39a034794d4648f0664ec33891236654c
Charles Schleich <schleich.cdaniel@gmail.com>
2023-09-23 00:12:15 +0200

    Add YoloBuilder, Yolo, YoloType, and InferenceResult structs, Add Apache License, update Cargo.toml

---
+++ scripts/download_v8_models.sh
@@ -0,0 +1,2 @@
+# TODO Download from https://github.com/ultralytics/assets/releases
+# Convert to Torchscript / Onnx / TF file format 
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,97 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use wasi_nn::{Error, ExecutionTarget, Graph, GraphEncoding};
+
+pub struct Yolo {
+    // inference_type: YoloType
+    graph: Graph,
+    classes: Vec<String>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct InferenceResult;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Yolo {
+    pub fn new(graph: Graph, classes: Vec<String>) -> Self {
+        Yolo { graph, classes }
+    }
+
+    pub fn infer() -> InferenceResult {
+        InferenceResult
+    }
+}
+
+#[derive(Debug)]
+pub enum BuildError {
+    MissingClasses,
+    GraphError(Error),
+}
+
+impl From<Error> for BuildError {
+    fn from(value: Error) -> Self {
+        BuildError::GraphError(value)
+    }
+}
+
+pub enum YoloType {
+    Pose,
+    Segment,
+    Detection,
+}
+
+pub struct YoloBuilder {
+    // inference_type: YoloType
+    graph_encoding: GraphEncoding,
+    execution_target: ExecutionTarget,
+    classes: Option<Vec<String>>,
+}
+
+impl YoloBuilder {
+    #[inline(always)]
+    pub fn new() -> Self {
+        YoloBuilder {
+            graph_encoding: GraphEncoding::Pytorch,
+            execution_target: ExecutionTarget::CPU,
+            classes: None,
+        }
+    }
+
+    #[inline(always)]
+    pub fn execution_target(mut self, execution_target: ExecutionTarget) -> Self {
+        self.execution_target = execution_target;
+        self
+    }
+
+    #[inline(always)]
+    pub fn graph_encoding(mut self, graph_encoding: GraphEncoding) -> Self {
+        self.graph_encoding = graph_encoding;
+        self
+    }
+
+    #[inline(always)]
+    pub fn build_from_bytes<B>(self, bytes_array: impl AsRef<[B]>) -> Result<Yolo, BuildError>
+    where
+        B: AsRef<[u8]>,
+    {
+        match self.classes {
+            Some(classes) => {
+                let graph = wasi_nn::GraphBuilder::new(self.graph_encoding, self.execution_target)
+                    .build_from_bytes(bytes_array)?;
+                Ok(Yolo::new(graph, classes))
+            }
+            None => Err(BuildError::MissingClasses),
+        }
+    }
+
+    #[inline(always)]
+    pub fn build_from_files<P>(self, files: impl AsRef<[P]>) -> Result<Yolo, BuildError>
+    where
+        P: AsRef<std::path::Path>,
+    {
+        match self.classes {
+            Some(classes) => {
+                let graph = wasi_nn::GraphBuilder::new(self.graph_encoding, self.execution_target)
+                    .build_from_files(files)?;
+                Ok(Yolo::new(graph, classes))
+            }
+            None => Err(BuildError::MissingClasses),
+        }
     }
 }
